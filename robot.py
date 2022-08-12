ff9e768affacb1ecec2850b980bd5387cfb14401
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-12 13:23:53 -0400

    Structured out the robot class and corrected the name in the weapon class.

--- robot.py
+++ robot.py
@@ -1,3 +1,10 @@
+from weapon import Weapon
+
 class Robot:
-    def __init__(self):
+    def __init__(self, name):
+        name = "Mecha Godzilla"
+        health = 100
+        active_weapon = Weapon()
+
+    def attack(self, dinosuar):
         pass
--- weapon.py
+++ weapon.py
@@ -1,4 +1,4 @@
 class Weapon:
     def __init__(self, name, attack_power):
-        name = "Mecha Godzilla"
+        name = "Mecha Sword"
         attack_power = ""
