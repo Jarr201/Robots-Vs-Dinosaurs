a054a191debd6ddf457a5dde5a9868d722996828
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-12 12:56:56 -0400

    Structured out the Battlefield class in battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -1,3 +1,19 @@
+from robot import Robot
+from dinosuar import Dinosuar
+
 class Battlefield:
     def __init__(self):
+        robot = Robot()
+        dinosuar = Dinosuar()
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
         pass
