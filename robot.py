544dcd5622cec515f0a945ef0fb0684ebd6affcd
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-15 12:10:34 -0400

    Omitted unnecessary lines of code after a review.

--- robot.py
+++ robot.py
@@ -1,12 +1,11 @@
 from weapon import Weapon
-from dinosuar import Dinosuar
 
 class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
         self.active_weapon = Weapon()
-        self.robot = Robot()
 
     def attack(self, dinosuar):
+        self.dinosuar = self.dino_target
         pass
--- weapon.py
+++ weapon.py
@@ -2,5 +2,3 @@ class Weapon:
     def __init__(self, name, attack_power):
         self.name = name
         self.attack_power = attack_power
-        self.weapon = Weapon()
-        self.weapon = ("Mecha Godzilla", 19)
